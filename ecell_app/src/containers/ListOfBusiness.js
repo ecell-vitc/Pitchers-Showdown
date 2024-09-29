844cac114a88d825120fe52141e79c755365f765
Pragati1910 <pragatimaheshwari04@gmail.com>
2024-09-29 15:48:32 +0530

    updated page

--- ecell_app/src/containers/ListOfBusiness.js
+++ ecell_app/src/containers/ListOfBusiness.js
@@ -1,73 +1,173 @@
-import React from "react"
-import Logo from "../images/logo.png"
+import React from "react";
+import Logo from "../images/logo.png";
 
 const Listofbusiness = () => {
-    return (
-        <>
-                <div class="top-bar">
-                    <img class="logo" src={Logo} />
-                </div>
-                <div class="content">
-                    <div class="Header">
-                        <div class="head">List of Businesses</div>
-                        <div class="Amount">Amount: 10CR</div>
-                    </div>
-                    
-                    <div class="buttons">
-                        <button class="Teams"><a href="/business_about/1">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/2">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/3">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/4">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/5">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/6">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/7">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/8">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/9">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/10">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/11">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/12">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/13">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/14">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/15">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/16">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/17">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/18">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/19">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/20">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/21">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/22">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/23">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/24">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/25">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/26">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/27">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/28">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/29">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/30">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/31">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/32">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/33">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/34">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/35">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/36">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/37">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/38">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/39">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/40">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/41">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/42">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/43">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/44">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/45">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/46">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/47">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/48">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/49">Team Name</a></button>
-                        <button class="Teams"><a href="/business_about/50">Team Name</a></button>
-                    </div>   
-                </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div class="top-bar">
+        <img class="logo" src={Logo} />
+      </div>
+      <div class="content">
+        <div class="Header">
+          <div class="head">List of Businesses</div>
+          <div class="Amount">Amount: 10CR</div>
+        </div>
 
-export default Listofbusiness
+        <div class="buttons">
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/1">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/2">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/3">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/4">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/5">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/6">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/7">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/8">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/9">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/10">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/11">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/12">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/13">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/14">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/15">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/16">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/17">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/18">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/19">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/20">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/21">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/22">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/23">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/24">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/25">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/26">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/27">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/28">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/29">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/30">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/31">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/32">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/33">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/34">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/35">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/36">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/37">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/38">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/39">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/40">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/41">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/42">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/43">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/44">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/45">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/46">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/47">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/48">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/49">Team Name</a>
+          </button>
+          <button class="Teams" style={{ border: "1px solid black" }}>
+            <a href="/business_about/50">Team Name</a>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Listofbusiness;
