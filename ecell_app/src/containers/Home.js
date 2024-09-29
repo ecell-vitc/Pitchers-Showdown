484f8138a154b5cd96ae20cac319bfb63714a47e
whyrupesh <srupeshkumar800@gmail.com>
2024-09-29 12:30:22 +0530

    added home file of dakshit

--- ecell_app/src/containers/Home.js
+++ ecell_app/src/containers/Home.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Home = () => {
+
+const Home= () => {
   return (
-    <div>
-      <h1>Welcome to Ecell</h1>
-      <p>This is the home page.</p>
+    <div className="container">
+      <div className="logos">
+        <img style={{ width: '100px', height: '9vh' }} src="./logo.png" alt="Logo" />
+        <img className="logo-text" style={{ width: '331px', height: '10vh' }} src="./ECELL LOGO TEXT.png" alt="Ecell Logo Text" />
+      </div>
+      <div className="content">
+        <h1>
+          Unleash your vision and conquer the world of business. <br /> Pitch, invest, and thrive at the <br /> ultimate
+        </h1>
+        <img className="gif" src="./standard.gif" alt="Pitch Showdown" /> <br /> <br />
+        <a href="#" className="btn">LOGIN</a>
+        <p>READY TO WIN BIG?</p>
+      </div>
     </div>
   );
-};
+}
 
 export default Home;
