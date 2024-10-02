f3e6dee2ccd4e9cacd4d79aa9f69160356404698
V-Srivatsan <vatsan.programming@gmail.com>
2024-10-02 10:11:50 +0000

    Added makeRequest util method

---
+++ client/src/utils.js
@@ -0,0 +1,14 @@
+const SERVER_URL = 'localhost:5000/api/'
+
+export async function makeRequest(method, url, data) {
+    const res = await fetch(SERVER_URL + url, {
+        cache: 'no-store',
+        method, mode: 'cors', headers: {
+            'x-access-token': (localStorage.getItem('token') || ''),
+            'content-type': (method == 'GET' ? '' : 'application/json')
+        },
+        body: (method == 'GET' ? null : JSON.stringify(data)),
+    })
+
+    return res.json()
+}
