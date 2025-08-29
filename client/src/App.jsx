946c188d14759493b63677a64e3f1e72ce95dc1d
K. Mahesh Chandran <kmahesh110606@outlook.com>
2025-08-29 11:41:30 +0530

    Updated Profile Page + Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './Login'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -13,6 +14,7 @@ function App() {
      <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
         {/* Add more routes here */}
       </Routes>
     </Router>
