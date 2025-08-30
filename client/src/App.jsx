3605bd4699094e30e702586b82831e94f1de14a5
GryffinX <g.ayush2k07@gmail.com>
2025-08-30 16:31:51 +0530

    Implemented Investing features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRouter';
 import Profile from './Profile'; //just taken as demo.
 import Login from './Login';
+import BusinessPage from './pages/Business/BusinessPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+        <Route path="business/:teamId" element={<BusinessPage />} />
       </Routes>    
     </BrowserRouter>
   )  
