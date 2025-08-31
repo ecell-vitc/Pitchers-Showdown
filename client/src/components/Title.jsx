213d2cbc913c07b8063bbb63541e80f118447b16
ecell-vitc <helloecellvitc@gmail.com>
2025-08-31 21:23:36 +0530

    Fixed login button

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import "../styles/Homepage.css";
 import topImage from '../../public/assets/overlay1.png'
+import { Link } from 'react-router-dom'
+
 function Title() {
   return (
     <div className="title-container">
       <div className="pitchers-text">THE PITCHERS</div>
       <div className="showdown-text">SHOWDOWN</div>
-      <button 
-        onClick={() => window.location.href = '/login'}
-        className="login-button"
-      >
-        Log in
-      </button>
+      <Link to='/login'>
+        <button className="login-button">
+          Log in
+        </button>
+      </Link>
     </div>
   );
 }
